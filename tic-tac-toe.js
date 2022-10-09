575462bb1cab618a6dddf1911f27965743bae566
KLAW4life <kerene789@gmail.com>
2022-10-09 02:14:27 -0500

    Checks for winner and updates status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }); 
 
     //Exercise 2
-    const WIN_COMBINATIONS = [
+    const win_combinations = [
         [0,1,2],
         [3,4,5],
         [6,7,8],
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     ];
 
     const board = Array.from(document.querySelectorAll('.square'));
-    //console.log(board);
+    console.log(board);
     let playerx = true;
     let playero = false;
 
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 sq.target.className += ' X'
                 playerx = false;
                 playero = true;
+                winner_check();
                 
             }
             else if (playero && sq.target.textContent == ''){
@@ -44,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 sq.target.className += ' O'
                 playerx = true;
                 playero = false;
+                winner_check();
             }
             
         }); 
@@ -59,6 +61,58 @@ document.addEventListener("DOMContentLoaded", function(event) {
             sq.target.classList.remove("hover");
         })
     });
+
+    //Exercise 4
+    
+    function winner_check(){
+        let win = false;
+        let winner = null;
+        let box0, box1, box2;
+
+        win_combinations.forEach((combo) =>{  //checking the boxes for their values of either 'X' or 'O'
+            
+
+            if (board[combo[0]].hasChildNodes()){
+                box0 = board[combo[0]].childNodes[0].textContent
+            }
+            else {
+                box0 = "";
+            }
+
+            if (board[combo[1]].hasChildNodes()){
+                box1 = board[combo[1]].childNodes[0].textContent
+            }
+            else {
+                box1 = "";
+            }
+
+            if (board[combo[2]].hasChildNodes()){
+                box2 = board[combo[2]].childNodes[0].textContent
+            }
+            else {
+                box2 = "";
+            }
+
+            if (box0 != "" && box1 != "" && box2 != ""){
+                //console.log(box0,box1,box2);
+
+                if (box0 == box1 && box1 == box2){
+                    win = true;
+                    //console.log(win);
+
+                    let winner = document.getElementById('status');
+                    winner.textContent = `Congradulations! ${box1} is the Winner!`
+                    winner.classList += "you-won";
+
+                }
+            }
+
+
+        });
+        //console.log(winner);
+        return winner;
+        
+    }
     
   })
 
